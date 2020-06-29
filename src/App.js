9c40bd4d74a6b01d99c1555edc09afbb7d3bd7b4
David Colatti <davidcolatti@gmail.com>
2020-06-29 10:39:37 -0400

    feat: pulling data from graphql server

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import React from "react";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "@apollo/react-hooks";
+
+import PokemonsContainer from "./containers/PokemonsContainer";
 
 const App = () => {
-  return <div>App</div>;
+  const client = new ApolloClient({
+    uri: "https://graphql-pokemon.now.sh/",
+  });
+
+  return (
+    <ApolloProvider client={client}>
+      <main>
+        <PokemonsContainer />
+      </main>
+    </ApolloProvider>
+  );
 };
 
 export default App;
---
+++ src/containers/PokemonsContainer.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { useQuery } from "@apollo/react-hooks";
+
+import { GET_POKEMONS } from "../graphql/get-pokemons";
+
+const PokemonsContainer = () => {
+  const { data: { pokemons = [] } = {} } = useQuery(GET_POKEMONS, {
+    variables: { first: 9 },
+  });
+
+  return (
+    <div className="pokemons">
+      {pokemons && pokemons.map((pokemon) => JSON.stringify(pokemon))}
+    </div>
+  );
+};
+
+export default PokemonsContainer;
